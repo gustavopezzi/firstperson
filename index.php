ae43c22b98cd8a5f98ce3234393528174f6e4fa1
unknown <intervene@dell7.dell7>
2015-07-27 10:20:07 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+    <head>
+        <title>First Person Shooter (Lab)</title>
+        <link rel="stylesheet" type="text/css" href="css/base.css">
+        <script type="text/javascript" src="js/fps.js"></script>
+    </head>
+    <body>
+        <canvas id='display'></canvas>
+    </body>
+</html>
